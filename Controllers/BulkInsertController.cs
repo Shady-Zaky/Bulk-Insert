94073b5649138d09f73b33b7c2f950644c5f863b
Shady Zaky <shady.zaki@moneyfellows.com>
2025-03-16 20:25:16 +0200

    update models

--- Controllers/BulkInsertController.cs
+++ Controllers/BulkInsertController.cs
@@ -66,21 +66,5 @@ namespace BulkInsertAPI.Controllers
         }
     }
 
-    public class WorkerZoneAssignmentRequest
-    {
-        [Name("worker_code")]
-        public string? WorkerCode { get; set; }
-
-        [Name("zone_code")]
-        public string? ZoneCode { get; set; }
-
-        [Name("assignment_date")]
-        public string? AssignmentDate { get; set; }
-    }
-
-    public class ErrorResponse
-    {
-        public object? Data { get; set; }
-        public Dictionary<string, string>? Error { get; set; }
-    }
+   
 }
---
+++ Models/BulkInsertModels.cs
@@ -0,0 +1,22 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace BulkInsertAPI.Models
+{
+    public class WorkerZoneAssignmentRequest
+    {
+        [Name("worker_code")]
+        public string? WorkerCode { get; set; }
+
+        [Name("zone_code")]
+        public string? ZoneCode { get; set; }
+
+        [Name("assignment_date")]
+        public string? AssignmentDate { get; set; }
+    }
+
+    public class ErrorResponse
+    {
+        public object? Data { get; set; }
+        public Dictionary<string, string>? Error { get; set; }
+    }
+}
